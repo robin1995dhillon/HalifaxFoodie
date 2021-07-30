d535fc2bc4b2b2ac3a82b8a3a62c29653a7d8ecd
Robinder Dhillon <rb802397@dal.ca>
2021-07-30 13:18:18 -0300

    Update Review.js for ML

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,11 +1,57 @@
 import React from 'react'
+import {Form,Button,Card} from 'react-bootstrap';
+import axios from 'axios';
 
-function Review() {
-    return (
-        <div>
-            <h1>welcome to review page</h1>
-        </div>
-    )
+
+
+class Review extends React.Component{
+
+    state ={
+        files: null,
+        recipe: ''
+    };
+    
+    handleUpload = async()=>{
+        const formData = new FormData();
+        formData.append("file", this.state.files);
+        var filename = this.state.files
+        console.log('in file upload')
+        console.log(filename)
+        const response = await axios.post("http://localhost:2000/upload", formData).then(result => {
+            console.log("done");
+        });
+        }
+
+    onChangeHandler = (event) =>{
+        console.log(event.target.files[0])
+        this.setState({files: event.target.files[0]})
+    }
+
+    getSimilarity = (event) => {
+        axios.get('http://localhost:2000/similarity').then((result) => {
+          this.setState({recipe: result.data.similarFile})
+        })
+      }
+    
+
+    render(){
+        return(<div className="container">
+            <Card>
+        <Form controlId="form1" >                 
+                    <Form.Group controlId="formUpload">
+                    <Form.Label style={{width:'150px',margintop:'500px'}}>Upload file</Form.Label>
+                    <Form.Control type="file" placeholder="upload file" style={{width:'500px'}} onChange={this.onChangeHandler} required/>
+                    </Form.Group>
+                    <Button variant="outline-success" type="button" size="lg" onClick={this.handleUpload}>Submit</Button>
+                    <Button variant="outline-success" type="button" size="lg" onClick={this.getSimilarity}> Check Similarity</Button>
+                    </Form>
+                    </Card>
+            <Card>
+            <Form.Label style={{width:'150px',margintop:'500px'}}>{this.state.recipe}</Form.Label>
+            </Card>
+    </div>
+        )
+    }
 }
 
-export default Review
+export default Review;
